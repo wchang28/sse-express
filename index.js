96b1895448b87cd02dca076dbc8604fa44dabe58
wchang28 <wchang28@hotmail.com>
2016-01-21 10:23:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = function(onInitEventStreaming, onStreamingClose) {
+	return (function(req, res) {
+		// init SSE
+		///////////////////////////////////////////////////////////////////////
+		// let request last as long as possible
+		req.socket.setTimeout(Infinity);
+		//send headers for event-stream connection
+		res.writeHead(200, {
+			'Content-Type': 'text/event-stream',
+			'Cache-Control': 'no-cache',
+			'Connection': 'keep-alive'
+		});
+		// add a sseSend() method to the result object
+		res.sseSend = function(data, event) {
+			if (event) res.write("event: " + event.toString() + "\n");
+			res.write("data: " + JSON.stringify(data) + "\n\n");
+		}
+		res.write('\n');
+		///////////////////////////////////////////////////////////////////////
+		
+		// initialize event streaming
+		///////////////////////////////////////////////////////////////////////
+		var v;
+		if (typeof onInitEventStreaming === 'function') v = onInitEventStreaming(req, res);
+		///////////////////////////////////////////////////////////////////////
+		
+		// The 'close' event is fired when a user closes their browser window.
+		req.on("close", function() {
+			if (typeof onStreamingClose === 'function') onStreamingClose(req, res, v);
+		});
+	});
+};
